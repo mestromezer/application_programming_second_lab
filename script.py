03641b22d2621cdc295d301b3d1a486ad1ccf042
mestromezer <global0908@mail.ru>
2022-10-05 10:53:57 +0400

    First jobstep is done
    -Dataset reader
    -CSV writer

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import csv
 import os
-from re import I
+
 
 class Comment:
     
@@ -8,12 +8,25 @@ class Comment:
         self.name = None
         self.comment = None
         self.mark = None
-
+        
+        
+def write_as_csv(file_path, dataset):
+    
+    file = open("dataset_csv.csv", "w", encoding='utf-8', newline='')
+    csv_file = csv.writer(file ,delimiter=';')
+    csv_file.writerow(["Absolute path","Relative path","Class"])
+    
+    for i in range(0,len(dataset)):
+        csv_file.writerow([f'{file_path}+dataset_csv.csv',f'.\\dataset_cvs.csv',f'{dataset[i].mark}'])
+     
+        
 def get_dataset(path):
     dataset=list()
+    
     for folder_num in range(1,6):
         folder_path = path+'\\'+str(folder_num)
         num_of_files = sum(os.path.isfile(os.path.join(folder_path, f)) for f in os.listdir(folder_path)) + 1
+        
         for file_num in range(1,num_of_files):
             path_to_file = folder_path+f'\\{(file_num):04}'+'.txt'
             try:
@@ -40,10 +53,13 @@ def get_dataset(path):
             comment.comment = text.strip()
             dataset.append(comment)
     return dataset
+    
             
 if __name__=='__main__':
     
     path='C:\\application_programming\\first_lab\\dataset'
     dataset = get_dataset(path)
+    
+    write_as_csv(path, dataset)
     
     print("Работа окончена")
